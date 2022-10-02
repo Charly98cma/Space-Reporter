66fae90c145c5927e0c1d2c7dea2cc51ebe6c86a
Carlos Miguel <1998cma@gmail.com>
2022-10-02 18:01:21 +0200

    feat: Sign Up basic implementation

--- app/src/main/java/com/dam/spacereporter/SignUpActivity.java
+++ app/src/main/java/com/dam/spacereporter/SignUpActivity.java
@@ -1,20 +1,135 @@
 package com.dam.spacereporter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-// TODO Implement
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
 public class SignUpActivity extends AppCompatActivity {
 
     private static final String tag = "com.dam.spacereporter.signup";
+
+    private static Pattern uppercase = Pattern.compile("[A-Z]");
+    private static Pattern lowercase = Pattern.compile("[a-z]");
+    private static Pattern digit = Pattern.compile("[0-9]");
+
+    EditText signup_txt_fullname, signup_txt_username, signup_txt_password, signup_txt_repeatpassword, signup_txt_email;
+    Button signup_btn_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Toast.makeText(this, "Feature not implemented", Toast.LENGTH_SHORT).show();
+        /*
+         * UI ELEMENTS
+         */
+
+        signup_txt_fullname = getEditText(R.id.signup_txt_fullname);
+        signup_txt_username = getEditText(R.id.signup_txt_username);
+        signup_txt_password = getEditText(R.id.signup_txt_password);
+        signup_txt_repeatpassword = getEditText(R.id.signup_txt_repeatpassword);
+        signup_txt_email = getEditText(R.id.signup_txt_email);
+
+        signup_btn_signup = findViewById(R.id.signup_btn_signup);
+
+        /*
+         * LISTENERS
+         */
+
+        signup_btn_signup.setOnClickListener(view -> {
+
+            // Read fields
+            final String fullname, username, password, repeatpassword, email;
+            fullname = readEditText(signup_txt_fullname);
+            username = readEditText(signup_txt_username);
+            password = readEditText(signup_txt_password);
+            repeatpassword = readEditText(signup_txt_repeatpassword);
+            email = readEditText(signup_txt_email);
+
+            // Check all fields are valid
+            if (!validFields(fullname, username, password, repeatpassword, email)) return;
+            if (!(validEmail(email) & validPassword(password, repeatpassword))) return;
+
+            // TODO Check user is not already registered (username and email)
+            // TODO Register new user if available email and usernames
+            Toast.makeText(SignUpActivity.this, "Sign Up not implemented", Toast.LENGTH_SHORT).show();
+
+            // Once registered, back to login
+            finish();
+        });
+    }
+
+    @NonNull
+    private EditText getEditText(int ID) {
+        return ((TextInputLayout) findViewById(ID)).getEditText();
+    }
+
+    @NonNull
+    private String readEditText(EditText field) {
+        return Objects.requireNonNull(field).getText().toString().trim();
+    }
+
+    private boolean validFields(
+            String fullname, String username, String pwd, String rpwd, String email
+    ) {
+        boolean valid = true;
+
+        if (fullname.isEmpty()) {
+            signup_txt_fullname.setError("Name required");
+            valid = false;
+        }
+
+        if (username.isEmpty()) {
+            signup_txt_username.setError("Username required");
+            valid = false;
+        }
+
+        if (email.isEmpty()) {
+            signup_txt_email.setError("Email required");
+            valid = false;
+        }
+
+        if (pwd.isEmpty()) {
+            signup_txt_password.setError("Password required");
+            valid = false;
+        }
+
+        if (rpwd.isEmpty()) {
+            signup_txt_repeatpassword.setError("Password required");
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    private boolean validEmail(@NonNull String email) {
+        boolean valid = Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        if (!valid)
+            signup_txt_email.setError("Invalid email address");
+        return valid;
+    }
+
+    private boolean validPassword(String pwd, String rpwd) {
+
+        if (!(uppercase.matcher(pwd).find() && lowercase.matcher(pwd).find() && digit.matcher(pwd).find() && pwd.length() >= 8)) {
+            signup_txt_password.setError(getResources().getString(R.string.signup_error_pwdrequirements));
+            return false;
+        }
+
+        if (!pwd.equals(rpwd)) {
+            signup_txt_repeatpassword.setError(getResources().getString(R.string.signup_error_nomatchpwd));
+            return false;
+        }
+        return true;
     }
 }
