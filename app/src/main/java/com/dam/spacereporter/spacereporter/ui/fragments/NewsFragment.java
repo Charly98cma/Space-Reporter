9d699db495c43491317871ba54facb980229c0c3
Carlos Miguel Alonso <1998cma@gmail.com>
2022-11-14 19:49:50 +0100

    feat: Fully functional News

--- app/src/main/java/com/dam/spacereporter/spacereporter/ui/fragments/NewsFragment.java
+++ app/src/main/java/com/dam/spacereporter/spacereporter/ui/fragments/NewsFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,14 +14,26 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.dam.spacereporter.R;
 import com.dam.spacereporter.spacereporter.data.models.Article;
 import com.dam.spacereporter.spacereporter.ui.news.NewsRVAdapter;
 import com.google.android.material.navigation.NavigationView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class NewsFragment extends Fragment {
+
+    // Number of articles to skip
+    private int skip = 0;
+    // Number of articles on each requests
+    private final int limit = 20;
 
     private int count = 0;
 
@@ -65,66 +78,47 @@ public class NewsFragment extends Fragment {
 
         // Init array list of articles
         newsArrayList = new ArrayList<>();
+        getData();
 
-        // TODO Replace with a call to retrieve initial data from the API
-        initData();
-
-        newsRV.setLayoutManager(new LinearLayoutManager(getContext()));
-
+        newsRV.setLayoutManager(new LinearLayoutManager(requireContext()));
         newsNestedScrollView.setOnScrollChangeListener((NestedScrollView.OnScrollChangeListener) (v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
             if (scrollY == v.getChildAt(0).getMeasuredHeight() - v.getMeasuredHeight()) {
                 count++;
                 newsProgressBar.setVisibility(View.VISIBLE);
                 // TODO Replace with a call to retrive more data from API
-                if (count < 20) initData();
+                if (count < 20) getData();
             }
         });
     }
 
-    private void initData() {
-
-        String[] titles = {
-                "Delayed NASA Venus mission looks for a reprieve",
-                "ESA seeks funding for navigation technology programs at ministerial",
-                "X-37B space plane completes its sixth mission, lands after nearly 30 months in orbit",
-                "SpaceX launches Falcon 9 booster into retirement on Intelsat mission",
-                "Falcon 9 B1051 makes final flight on Galaxy-31 & 32 mission",
-                "NASA moving ahead with Nov. 16 Artemis 1 launch attempt",
-                "NASA Sets New Coverage for Artemis I Moon Mission Launch",
-                "NASA says its SLS rocket is good to go for a launch attempt next Wednesday",
-                "Starfish books launch for in-orbit satellite docking mission next fall",
-                "Space Force orders new weather satellite from Ball Aerospace"
-        };
-
-        String[] imgUrl = {
-                "https://spacenews.com/wp-content/uploads/2022/11/veritas-2022.jpg",
-                "https://spacenews.com/wp-content/uploads/2022/11/galileo-cx.jpg",
-                "https://spacenews.com/wp-content/uploads/2022/11/221111-F-XX000-0002-scaled.jpg",
-                "https://spaceflightnow.com/wp-content/uploads/2022/11/DSC_8650.jpg",
-                "https://www.nasaspaceflight.com/wp-content/uploads/2022/11/FhYGiOYWAAAp-RE-1170x780.jpeg",
-                "https://spacenews.com/wp-content/uploads/2022/11/art1-221111.jpg",
-                "https://www.nasa.gov/sites/default/files/thumbnails/image/nhq202211110003.jpg?itok=GBXe0GR2",
-                "https://cdn.arstechnica.net/wp-content/uploads/2022/09/Artemis-I-LA1-Aug-2022-3042.jpg",
-                "https://spacenews.com/wp-content/uploads/2022/11/Starfish-Otter-Pup-scaled.jpg",
-                "https://spacenews.com/wp-content/uploads/2021/05/WSF-M-Mission_1018_sm.jpg"
-        };
-
-        String[] urls = {
-                "https://spacenews.com/delayed-nasa-venus-mission-looks-for-a-reprieve/",
-                "https://spacenews.com/esa-seeks-funding-for-navigation-technology-programs-at-ministerial/",
-                "https://spacenews.com/x-37b-space-plane-completes-its-sixth-mission-lands-after-nearly-30-months-in-orbit/",
-                "https://spaceflightnow.com/2022/11/12/falcon-9-galaxy-31-32-live-coverage/",
-                "https://www.nasaspaceflight.com/2022/11/galaxy-31-32/",
-                "https://spacenews.com/nasa-moving-ahead-with-nov-16-artemis-1-launch-attempt/",
-                "http://www.nasa.gov/press-release/nasa-sets-new-coverage-for-artemis-i-moon-mission-launch",
-                "https://arstechnica.com/science/2022/11/nasa-says-its-sls-rocket-is-good-to-go-for-a-launch-attempt-next-wednesday/",
-                "https://spacenews.com/starfish-books-launch-for-in-orbit-satellite-docking-mission-next-fall/",
-                "https://spacenews.com/space-force-orders-new-weather-satellite-from-ball-aerospace/"
-        };
-
-        for (int i = 0; i < titles.length; i++) {
-            newsArrayList.add(new Article(i, titles[i], urls[i], imgUrl[i], "", ""));
-            newsRV.setAdapter(new NewsRVAdapter(getContext(), newsArrayList));
-        }
+    private void getData() {
+        RequestQueue queue = Volley.newRequestQueue(requireContext());
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                String.format(getString(R.string.api_url), limit, skip),
+                null,
+                response -> {
+                    try {
+                        // Add the retrieved articles to the skip
+                        skip += limit;
+                        // Read the new articles
+                        newsRV.setVisibility(View.VISIBLE);
+                        // TODO Change to serialize the objects
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject responseObj = response.getJSONObject(i);
+                            newsArrayList.add(new Article(
+                                    responseObj.getInt("id"),
+                                    responseObj.getString("title"),
+                                    responseObj.getString("url"),
+                                    responseObj.getString("imageUrl"),
+                                    responseObj.getString("newsSite"),
+                                    responseObj.getString("summary")));
+                            newsRV.setAdapter(new NewsRVAdapter(requireContext(), newsArrayList));
+                        }
+                    }catch (JSONException e) {
+                        e.printStackTrace();
+                    }},
+                error -> Toast.makeText(requireContext(), R.string.news_toast_errorApi, Toast.LENGTH_SHORT).show());
+        queue.add(jsonArrayRequest);
     }
 }
