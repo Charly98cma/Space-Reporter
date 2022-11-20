ad649f39b72f57c8a861b28ebc38e158e6effef1
Carlos Miguel Alonso <1998cma@gmail.com>
2022-11-20 16:09:07 +0100

    fix: Added wait for user info

--- app/src/main/java/com/dam/spacereporter/spacereporter/ui/auth/login/LoginActivity.java
+++ app/src/main/java/com/dam/spacereporter/spacereporter/ui/auth/login/LoginActivity.java
@@ -68,6 +68,12 @@ public class LoginActivity extends AppCompatActivity {
             if (loginResult.getSuccess()) {
                 setResult(RESULT_OK);
                 loginViewModel.cacheUserInfo(this);
+                // Wait to have user information on SharedPreferences
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 Toast.makeText(this, R.string.login_toast_success, Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(this, MainActivity.class));
                 finish();
