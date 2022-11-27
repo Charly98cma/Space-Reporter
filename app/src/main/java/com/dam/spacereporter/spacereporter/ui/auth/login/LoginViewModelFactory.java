2adf8d6197dcdb1e471b8783b8cffd6d63033056
Carlos Miguel Alonso <1998cma@gmail.com>
2022-11-27 11:08:52 +0100

    docs: Documented some functions

--- app/src/main/java/com/dam/spacereporter/spacereporter/ui/auth/login/LoginViewModelFactory.java
+++ app/src/main/java/com/dam/spacereporter/spacereporter/ui/auth/login/LoginViewModelFactory.java
@@ -4,16 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+/**
+ * Factory to create the LoginViewModel and pass arguments (if necessary) to its constructor
+ */
 public class LoginViewModelFactory implements ViewModelProvider.Factory {
 
     @NonNull
     @Override
     @SuppressWarnings("unchecked")
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class))
             return (T) new LoginViewModel();
-        } else {
+        else
             throw new IllegalArgumentException("Unknown ViewModel class");
-        }
     }
 }
